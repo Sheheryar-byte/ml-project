7593eb5b1c32c2e5eb893a07c514c02765cc4e45
Sheheryar-byte <thesheheryarahmad767@gmail.com>
2025-04-04 01:55:26 -0700

    Data Transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ from src.exception import CustomException
 from dataclasses import dataclass
 import pandas as pd
 import numpy as np
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,5 +52,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
